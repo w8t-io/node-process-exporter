115463a73507cf32dba8ec16c7b8a468891af821
Cairry <115769353+Cairry@users.noreply.github.com>
2025-06-27 15:39:54 +0800

    🚀 Add process disk io monitor and update dashboard

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/prometheus/client_golang v1.14.0
-	github.com/shirou/gopsutil v3.21.11+incompatible
+	github.com/shirou/gopsutil/v3 v3.24.5
 	github.com/sirupsen/logrus v1.9.3
 )
 
@@ -13,10 +13,13 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.41.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/tklauser/go-sysconf v0.3.14 // indirect
 	github.com/tklauser/numcpus v0.8.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
--- process.go
+++ process.go
@@ -1,186 +1,193 @@
 package main
 
 import (
+	"net/http"
+	"os"
+	"strconv"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/process"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/process"
 	"github.com/sirupsen/logrus"
-	"log"
-	"net/http"
-	"strconv"
 )
 
-type ProcessGaugeCollect struct {
-	// 进程 cpu
-	CPUGaugeCollect *prometheus.Desc
-	// 进程 memory
-	MemoryGaugeCollect *prometheus.Desc
-	// 进程 文件打开数
-	OpenFilesGaugeCollect *prometheus.Desc
+const (
+	namespace = "node"
+	subsystem = "process"
+)
+
+// 定义指标的标签
+var processLabels = []string{"name", "pid", "cmd", "user"}
+
+// ProcessCollector 实现了 prometheus.Collector 接口
+type ProcessCollector struct {
+	CPU             *prometheus.Desc
+	Memory          *prometheus.Desc
+	OpenFiles       *prometheus.Desc
+	ReadBytesTotal  *prometheus.Desc
+	WriteBytesTotal *prometheus.Desc
 }
 
-func NewProcessGaugeCollect() *ProcessGaugeCollect {
-	return &ProcessGaugeCollect{
-		CPUGaugeCollect: prometheus.NewDesc(
-			"describe_node_process_cpu_info",
-			"node process cpu monitor",
-			[]string{"name", "pid", "cmd", "user"},
+// NewProcessCollector 创建一个新的 ProcessCollector
+func NewProcessCollector() *ProcessCollector {
+	return &ProcessCollector{
+		CPU: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "cpu_usage_percent"),
+			"Process CPU usage percentage.",
+			processLabels,
 			nil,
 		),
-
-		MemoryGaugeCollect: prometheus.NewDesc(
-			"describe_node_process_memory_info",
-			"node process memory monitor",
-			[]string{"name", "pid", "cmd", "user"},
+		Memory: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "memory_usage_percent"),
+			"Process memory usage percentage.",
+			processLabels,
 			nil,
 		),
-
-		OpenFilesGaugeCollect: prometheus.NewDesc(
-			"describe_node_process_openfiles_info",
-			"node process openfiles monitor",
-			[]string{"name", "pid", "cmd", "user"},
+		OpenFiles: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "open_files_count"),
+			"Number of open files by the process.",
+			processLabels,
+			nil,
+		),
+		ReadBytesTotal: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "read_bytes_total"),
+			"Total number of bytes read by the process.",
+			processLabels,
+			nil,
+		),
+		WriteBytesTotal: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, subsystem, "write_bytes_total"),
+			"Total number of bytes written by the process.",
+			processLabels,
 			nil,
 		),
 	}
 }
 
-func (pgc ProcessGaugeCollect) Describe(docs chan<- *prometheus.Desc) {
-	docs <- pgc.CPUGaugeCollect
-	docs <- pgc.MemoryGaugeCollect
-	docs <- pgc.OpenFilesGaugeCollect
+// Describe 将所有指标的描述符发送到提供的 channel
+func (pc *ProcessCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- pc.CPU
+	ch <- pc.Memory
+	ch <- pc.OpenFiles
+	ch <- pc.ReadBytesTotal
+	ch <- pc.WriteBytesTotal
 }
 
-func (pgc ProcessGaugeCollect) Collect(metric chan<- prometheus.Metric) {
+// Collect 收集所有进程的指标数据
+func (pc *ProcessCollector) Collect(ch chan<- prometheus.Metric) {
 	processes, err := process.Processes()
 	if err != nil {
-		log.Fatal(err)
+		logrus.Errorf("Failed to get processes: %v", err)
 		return
 	}
 
 	for _, proc := range processes {
-		registerMetric(proc, proc.Pid, metric, pgc)
+		pid := proc.Pid
+		name, err := proc.Name()
+		if err != nil {
+			logrus.Debugf("Failed to get name for PID %d, err: %v", pid, err)
+			continue
+		}
+
+		cmdline, err := proc.Cmdline()
+		if err != nil {
+			logrus.Debugf("Failed to get cmdline for PID %d (%s), err: %v", pid, name, err)
+			cmdline = ""
+		}
+
+		user, err := proc.Username()
+		if err != nil {
+			logrus.Debugf("Failed to get username for PID %d (%s), err: %v", pid, name, err)
+			user = "unknown"
+		}
+
+		// 创建标签值
+		labelValues := []string{name, strconv.Itoa(int(pid)), cmdline, user}
+
+		// 获取并注册 CPU 指标
+		if cpuPercent, err := proc.CPUPercent(); err == nil {
+			if cpuPercent > 0 {
+				ch <- prometheus.MustNewConstMetric(pc.CPU, prometheus.GaugeValue, cpuPercent, labelValues...)
+			}
+		} else {
+			logrus.Debugf("Failed to get CPU usage for PID %d (%s), err: %v", pid, name, err)
+		}
+
+		// 获取并注册内存指标
+		if memPercent, err := getProcMemoryPercent(proc); err == nil {
+			if memPercent > 0 {
+				ch <- prometheus.MustNewConstMetric(pc.Memory, prometheus.GaugeValue, memPercent, labelValues...)
+			}
+		} else {
+			logrus.Debugf("Failed to get memory usage for PID %d (%s), err: %v", pid, name, err)
+		}
+
+		// 获取并注册文件打开数指标
+		if openFiles, err := proc.OpenFiles(); err == nil {
+			count := len(openFiles)
+			if count > 0 {
+				ch <- prometheus.MustNewConstMetric(pc.OpenFiles, prometheus.GaugeValue, float64(count), labelValues...)
+			}
+		} else {
+			logrus.Debugf("Failed to get open files for PID %d (%s), err: %v", pid, name, err)
+		}
+
+		// 获取并注册磁盘读写
+		if ioCounters, err := proc.IOCounters(); err == nil {
+			ch <- prometheus.MustNewConstMetric(pc.ReadBytesTotal, prometheus.CounterValue, float64(ioCounters.ReadBytes), labelValues...)
+			ch <- prometheus.MustNewConstMetric(pc.WriteBytesTotal, prometheus.CounterValue, float64(ioCounters.WriteBytes), labelValues...)
+		} else {
+			logrus.Debugf("Failed to get IO counters for PID %d (%s), err: %v", pid, name, err)
+		}
 	}
 }
 
-func registerMetric(proc *process.Process, pid int32, metric chan<- prometheus.Metric, pgc ProcessGaugeCollect) {
-	user, err := proc.Username()
-	if err != nil {
-		user = "unknown"
-	}
-
-	name, err := proc.Name()
-	if err != nil {
-		return
-	}
-
-	cpu, err := getProcCPU(proc)
-	if err != nil {
-		return
-	}
-
-	mem, err := getProcMEM(proc)
-	if err != nil {
-		return
-	}
-
-	openFiles, err := getProcOpenFileNumber(proc)
-	if err != nil {
-		return
-	}
-
-	cmdline, err := proc.Cmdline()
-	if err != nil {
-		return
-	}
-
-	if cpu != 0 {
-		metric <- prometheus.MustNewConstMetric(
-			pgc.CPUGaugeCollect,
-			prometheus.GaugeValue,
-			cpu,
-			name,
-			strconv.Itoa(int(pid)),
-			cmdline,
-			user,
-		)
-	}
-
-	if mem != 0 {
-		metric <- prometheus.MustNewConstMetric(
-			pgc.MemoryGaugeCollect,
-			prometheus.GaugeValue,
-			mem,
-			name,
-			strconv.Itoa(int(pid)),
-			cmdline,
-			user,
-		)
-	}
-
-	if openFiles != 0 {
-		metric <- prometheus.MustNewConstMetric(
-			pgc.OpenFilesGaugeCollect,
-			prometheus.GaugeValue,
-			float64(openFiles),
-			name,
-			strconv.Itoa(int(pid)),
-			cmdline,
-			user,
-		)
-	}
-}
-
-func main() {
-	procMetric := NewProcessGaugeCollect()
-	registry := prometheus.NewRegistry()
-	registry.MustRegister(procMetric)
-
-	http.HandleFunc("/metrics", func(writer http.ResponseWriter, request *http.Request) {
-		promhttp.HandlerFor(registry,
-			promhttp.HandlerOpts{ErrorHandling: promhttp.ContinueOnError}).ServeHTTP(writer, request)
-	})
-
-	logrus.Info("Service started!")
-
-	if err := http.ListenAndServe(":"+"9002", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func getProcCPU(proc *process.Process) (float64, error) {
-	cpuPercent, err := proc.CPUPercent()
-	if err != nil {
-		logrus.Errorf(err.Error())
-		return 0, err
-	}
-
-	return cpuPercent, nil
-}
-
-func getProcMEM(proc *process.Process) (float64, error) {
+// getProcMemoryPercent 计算单个进程的内存使用百分比
+func getProcMemoryPercent(proc *process.Process) (float64, error) {
 	procMem, err := proc.MemoryInfo()
 	if err != nil {
-		logrus.Errorf(err.Error())
 		return 0, err
 	}
 
 	nodeMem, err := mem.VirtualMemory()
 	if err != nil {
-		logrus.Errorf(err.Error())
 		return 0, err
 	}
 
-	// 乘以 100，得出正确的百分比
-	procMemBytes := procMem.RSS * 100
-	return float64(procMemBytes) / float64(nodeMem.Total), nil
+	// 进程内存使用率 = (进程使用的物理内存 / 节点总物理内存) * 100
+	return (float64(procMem.RSS) / float64(nodeMem.Total)) * 100.0, nil
 }
 
-func getProcOpenFileNumber(proc *process.Process) (int, error) {
-	files, err := proc.OpenFiles()
-	if err != nil {
-		return 0, err
+func main() {
+	var level logrus.Level
+	ll := os.Getenv("LOG_LEVEL")
+	if ll == "debug" {
+		level = logrus.DebugLevel
+	} else {
+		level = logrus.InfoLevel
 	}
 
-	return len(files), nil
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetLevel(level)
+
+	// 创建自定义采集器并注册
+	procCollector := NewProcessCollector()
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(procCollector)
+
+	// 创建 HTTP 处理器
+	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{
+		ErrorHandling: promhttp.ContinueOnError,
+	})
+
+	http.Handle("/metrics", handler)
+
+	addr := ":9002"
+	logrus.Infof("Service started! Listening on %s", addr)
+
+	// 启动 HTTP 服务
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		logrus.Fatalf("Failed to start HTTP server: %v", err)
+	}
 }
